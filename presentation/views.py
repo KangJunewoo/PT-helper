0f14c4b7e075312e26b69e068c4da7fe63a9108e
ruthetum <ruthetum@gmail.com>
2020-03-30 16:49:07 +0900

    pdf 이미지 변환 함수 추가

--- presentation/views.py
+++ presentation/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from pdf2image import convert_from_path, convert_from_bytes
+from pdf2image.exceptions import (
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
 
-# Create your views here.
+def pdfToImages():
+    images = convert_from_path('example.pdf')
+    i = 1
+    for image in images:
+	    image.save('image' + str(i) + '.jpg', 'JPEG')
+	    i = i + 1
